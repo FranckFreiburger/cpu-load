17ca4617d18cc971877f0b64a034b0d9a38dbe8f
Franck Freiburger <franck.freiburger@gmail.com>
2017-02-24 14:23:26 +0100

    - add test

---
+++ test/test.js
@@ -0,0 +1,24 @@
+const ProcessCpuLoad = require('..');
+
+var load = new ProcessCpuLoad;
+
+load.start(function(pcent) {
+
+    console.log('process CPU load:', pcent+'%');
+});
+
+setTimeout(function() {
+
+    load.stop();
+}, 5000);
+
+
+function makeCpuLoad() {
+
+    for ( var i = 0; i < 2000000; ++i )
+        Math.random();
+
+    setTimeout(makeCpuLoad, 0);
+}
+
+makeCpuLoad();
